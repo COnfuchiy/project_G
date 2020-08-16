47ef1f35df073d3cd8281e6926662629d7dfa84d
Dima <semm0202@yandex.ru>
2020-08-16 17:30:11 +0300

    check yourself

--- data/sprites.js
+++ data/sprites.js
@@ -5,13 +5,16 @@
 let player = Crafty.e('2D, Canvas, Color, Twoway, Gravity,Collision,Motion')
     .attr({x: 0, y: 0, z:12, w: 30, h: 30})
     .color('#F00')
-    .twoway(400,700)
+    .twoway(400,650)
     .gravity('Floor')
     .gravityConst(2050)
     .onHit("Floor", function(e) {
-        if(e[0].obj.y<this.y)
-            this.y-=this.y-e[0].obj.y;
-        this.resetMotion();
+        // if(e[0].obj.y<this.y)
+        //     this.y-=this.y-e[0].obj.y;
+        // this.resetMotion();
+        let to_ground = e[0].obj;
+        to_ground.removeComponent('Floor');
+        setTimeout(()=>to_ground.addComponent('Floor'),200);
     });
 Crafty.e('2D, Canvas, Color,Floor')
     .attr({x: 0, y: 500, w: document.documentElement.clientWidth, h: 20})
