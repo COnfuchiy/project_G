4ae04fe0a4dfcc1696cffb9a5de611e8569a82dc
Dima <semm0202@yandex.ru>
2020-08-18 18:59:02 +0300

    sprite items! vivat yuri-yuri!

--- data/items.js
+++ data/items.js
@@ -2,29 +2,26 @@
  * Created by Dima on 17.08.2020.
  */
 
-class Item{
-    constructor(type, platforms_width,height, sprite,score, color){
+class Item {
+    constructor(type, platforms_width, height, sprite, score) {
         this._type = type;
         this._platforms_width = platforms_width;
         this._height = height;
         this._sprites = sprite;
         this._score = score;
-        this._att_name = 'GEN_item';
-        this._color = color;
         return this;
     }
-    set_item(x){
+
+    set_item(x) {
         let item_score = this._score;
-        Crafty.e('2D, Canvas, Collision, Color,'+this._att_name)
+        let selected_sprite = Array.isArray(this._sprites)? this._sprites[getRandomInt(this._sprites.length)]:this._sprites;
+        Crafty.e('2D, Canvas, Collision,' + selected_sprite.name)
             .attr({
-                x: x,
-                y: this._height-40,
-                w:20,
-                h:20
+                x: x-parseInt(selected_sprite.w/2),
+                y: this._height - 10 - selected_sprite.h,
             })
-            .color(this._color)
-            .onHit('player',function(e) {
-                user_score+=item_score;
+            .onHit('player', function (e) {
+                user_score += item_score;
                 user_score_text.text(user_score.toString());
                 this.destroy();
             })
@@ -34,104 +31,212 @@ class Item{
                     this.destroy();
             });
     }
-    drop(){
-        if (getRandomInt(99)+1>=85 && this._platforms_width>180){
+
+    drop() {
+        if (getRandomInt(99) + 1 >= 85 && this._platforms_width > 180) {
             // double drop
             let first_item = this.calc_x_drop();
             let second_item = 0;
-            if (first_item>267){
-                second_item = Platforms.level_x+45-10+getRandomInt(1)*90;
+            if (first_item > 267) {
+                second_item = Platforms.level_x + 45 + getRandomInt(1) * 90;
             }
-            else{
-                if (this._platforms_width===267)
-                    second_item = Platforms.level_x+45-10+180;
+            else {
+                if (this._platforms_width === 267)
+                    second_item = Platforms.level_x + 45 + 180;
                 else
-                    second_item = Platforms.level_x+45-10+180+getRandomInt(1)*90;
+                    second_item = Platforms.level_x + 45 + 180 + getRandomInt(1) * 90;
             }
             this.set_item(first_item);
             this.set_item(second_item);
-        }else{
+        } else {
             let item_x = this.calc_x_drop();
             this.set_item(item_x);
         }
 
     }
-    calc_x_drop(){
-        switch (this._platforms_width){
+
+    calc_x_drop() {
+        switch (this._platforms_width) {
             case 93:
-                return Platforms.level_x+47-10;
+                return Platforms.level_x + 47;
             case 180:
-                return Platforms.level_x+45-10+getRandomInt(1)*90;
+                return Platforms.level_x + 45 + getRandomInt(1) * 90;
             case 267:
-                return Platforms.level_x+45-10+getRandomInt(2)*90;
+                return Platforms.level_x + 45 + getRandomInt(2) * 90;
             case 354:
-                return Platforms.level_x+45-10+getRandomInt(3)*90;
+                return Platforms.level_x + 45 + getRandomInt(3) * 90;
         }
 
     }
 
 }
 
-class ItemDrop{
+class ItemDrop {
     static item_types = [
         {
-            color:'#ffd200',
-            score:100,
-            chance:15,
+            sprites: [
+                {
+                    name: 'proc',
+                    w: 40,
+                    h: 40,
+                },
+                {
+                    name: 'proc_2',
+                    w: 40,
+                    h: 40,
+                },
+                {
+                    name: 'proc_3',
+                    w: 42,
+                    h: 40,
+                },
+                {
+                    name: 'proc_4',
+                    w: 42,
+                    h: 40,
+                }
+            ],
+            score: 100,
+            chance: 10,
         },
         {
-            color:'#0002ff',
-            score:200,
-            chance:30,
+            sprites: [
+                {
+                    name: 'motherboard',
+                    w: 38,
+                    h: 40,
+                },
+                {
+                    name: 'motherboard_2',
+                    w: 40,
+                    h: 40,
+                },
+                {
+                    name: 'motherboard_3',
+                    w: 40,
+                    h: 42,
+                }
+            ],
+            score: 200,
+            chance: 20,
         },
         {
-            color:'#aa5661',
-            score:300,
-            chance:45,
+            sprites: [
+                {
+                    name: 'fdisk',
+                    w: 37,
+                    h: 40,
+                },
+                {
+                    name: 'fdisk_2',
+                    w: 43,
+                    h: 40,
+                },
+            ],
+            score: 300,
+            chance: 30,
         },
         {
-            color:'#53346e',
-            score:400,
-            chance:60,
+            sprites: {
+                name: 'cooler',
+                w: 51,
+                h: 40,
+            },
+            score: 400,
+            chance: 40,
         },
         {
-            color:'#ff3200',
-            score:500,
-            chance:75,
+            sprites: {
+                name: 'disk_driver',
+                w: 57,
+                h: 40,
+            },
+            score: 500,
+            chance: 50,
         },
         {
-            color:'#aa5661',
-            score:600,
-            chance:90,
+            sprites: {
+                name: 'graphics',
+                w: 68,
+                h: 40,
+            },
+            score: 600,
+            chance: 60,
         },
         {
-            color:'#c4bfb2',
-            score:700,
-            chance:95,
+            sprites: {
+                name: 'hdd',
+                w: 31,
+                h: 40,
+            },
+            score: 700,
+            chance: 70,
         },
         {
-            color:'#388533',
-            score:800,
-            chance:99,
+            sprites: {
+                name: 'headphones',
+                w: 42,
+                h: 40,
+            },
+            score: 800,
+            chance: 80,
+        },
+        {
+            sprites: {
+                name: 'loudspeaker',
+                w: 38,
+                h: 40,
+            },
+            score: 500,
+            chance: 90,
+        },
+        {
+            sprites: {
+                name: 'mouse',
+                w: 29,
+                h: 40,
+            },
+            score: 600,
+            chance: 94,
+        },
+        {
+            sprites: {
+                name: 'usb',
+                w: 59,
+                h: 40,
+            },
+            score: 700,
+            chance: 98,
+        },
+        {
+            sprites: {
+                name: 'wired_keyboard',
+                w: 47,
+                h: 40,
+            },
+            score: 800,
+            chance: 100,
         },
 
     ];
     static chance_drop = 80;
 
-    static get_drop(platforms_width,height){
-        if (ItemDrop.check_drop() && is_active_item){
+    static get_drop(platforms_width, height) {
+        if (ItemDrop.check_drop() && is_active_item) {
             let dropped_item = ItemDrop.get_type_of_drop();
-            (new Item(1,platforms_width,height,1,dropped_item.score,dropped_item.color)).drop();
+            (new Item(1, platforms_width, height,  dropped_item.sprites, dropped_item.score)).drop();
         }
     }
-    static check_drop(){
-        return ItemDrop.chance_drop > (getRandomInt(99)+1)
+
+    static check_drop() {
+        return ItemDrop.chance_drop > (getRandomInt(99) + 1)
     }
-    static get_type_of_drop(){
-        let random_num = getRandomInt(99)+1;
+
+    static get_type_of_drop() {
+        let random_num = getRandomInt(99) + 1;
         let last_item = ItemDrop.item_types[0];
-        for (let i=0;i<ItemDrop.item_types.length;i++){
-            if (random_num<=ItemDrop.item_types[i].chance)
+        for (let i = 0; i < ItemDrop.item_types.length; i++) {
+            if (random_num <= ItemDrop.item_types[i].chance)
                 return last_item;
             else
                 last_item = ItemDrop.item_types[i];
--- data/sprites.js
+++ data/sprites.js
@@ -7,6 +7,25 @@ Crafty.sprite(267,20,"stok/plat3.png", {platx3:[0,0]});
 Crafty.sprite(354,20,"stok/plat4.png", {platx4:[0,0]});
 Crafty.sprite(66,30,"stok/cd.png",{cd:[0,0]});
 Crafty.sprite(61,50,"stok/cd-pc.png",{cd_exchanger:[0,0]});
+// item sprites
+Crafty.sprite(51,40,"sprites/items/cooler.png",{cooler:[0,0]});
+Crafty.sprite(57,40,"sprites/items/disk_driver.png",{disk_driver:[0,0]});
+Crafty.sprite(37,40,"sprites/items/fdisk.png",{fdisk:[0,0]});
+Crafty.sprite(43,40,"sprites/items/fdisk_2.png",{fdisk_2:[0,0]});
+Crafty.sprite(68,40,"sprites/items/graphics.png",{graphics:[0,0]});
+Crafty.sprite(31,40,"sprites/items/hdd.png",{hdd:[0,0]});
+Crafty.sprite(42,40,"sprites/items/headphones.png",{headphones:[0,0]});
+Crafty.sprite(38,40,"sprites/items/loudspeaker.png",{loudspeaker:[0,0]});
+Crafty.sprite(38,40,"sprites/items/motherboard.png",{motherboard:[0,0]});
+Crafty.sprite(40,40,"sprites/items/motherboard_2.png",{motherboard_2:[0,0]});
+Crafty.sprite(40,40,"sprites/items/motherboard_3.png",{motherboard_3:[0,0]});
+Crafty.sprite(29,40,"sprites/items/mouse.png",{mouse:[0,0]});
+Crafty.sprite(40,40,"sprites/items/proc.png",{proc:[0,0]});
+Crafty.sprite(40,40,"sprites/items/proc_2.png",{proc_2:[0,0]});
+Crafty.sprite(42,40,"sprites/items/proc_3.png",{proc_3:[0,0]});
+Crafty.sprite(42,40,"sprites/items/proc_4.png",{proc_4:[0,0]});
+Crafty.sprite(59,40,"sprites/items/usb.png",{usb:[0,0]});
+Crafty.sprite(47,40,"sprites/items/wired_keyboard.png",{wired_keyboard:[0,0]});
 // cd num field
 Crafty.e('2D, Canvas, cd')
     .attr({x:450, y:525});
